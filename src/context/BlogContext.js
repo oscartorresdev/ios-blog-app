e43807b7751094bac8b29aee7a9a97136c7acadf
oscartorresdev <johnsmith93030@gmail.com>
2020-01-13 17:52:39 +0800

    mock redux with context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider value={5}>{children}</BlogContext.Provider>;
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}`}];
+    default:
+      return state;
+  }
 };
+
+const addBlogPost = dispatch => {
+  return () => dispatch({ type: 'add_blogpost' });
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
